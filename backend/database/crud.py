11d775928177faf4db3cfc0de56177aa897b2782
Eren <erennakdag10@gmail.com>
2022-03-18 20:59:38 +0100

    added hashed passwords

--- backend/database/crud.py
+++ backend/database/crud.py
@@ -1,5 +1,6 @@
 from sqlalchemy.orm import Session
 from . import models, schemas
+from werkzeug.security import check_password_hash, generate_password_hash
 
 def get_user_by_id(db: Session, user_id: int) -> schemas.User:
     return db.query(models.User).filter(models.User.id == user_id).first()
@@ -13,7 +14,7 @@ def get_user_by_username(db: Session, username: str) -> schemas.User:
 def create_user(db: Session, user: schemas.UserCreate) -> schemas.User:
     user_data = user.dict()
     
-    # TODO: hash the password
+    user_data['password'] = generate_password_hash(user_data['password'])
     
     new_user = models.User(**user_data)
     db.add(new_user)
